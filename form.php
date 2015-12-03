dffe1f234ddb98c9e3ee5a5b364364046096b8b5
N0un <antoinelavier1@gmail.com>
2015-12-03 22:58:52 +0100

    ajout de form.php

--- form.php
+++ form.php
@@ -9,8 +9,8 @@ else {
 	$titre = mysqli_real_escape_string($_POST['titre']);
 	$message = mysqli_real_escape_string($_POST['message']);
 	$localisation = mysqli_real_escape_string($_POST['localisation']);
-	$date_evenement = mysqli_real_escape_string($POST_['date_evenement']);
-	$heure_evenement = mysqli_real_escape_string($_POST['heure_evenement']);
+	$date_evenement = mysqli_real_escape_string($POST_['date-evenement']);
+	$heure_evenement = mysqli_real_escape_string($_POST['heure-evenement']);
 	$niveau = mysqli_real_escape_string($_POST['niveau']);
 	
 	echo $titre . " " . $message . " " . $localisation . " " . $date_evenement . " " . $date_evenement . " " . $heure_evenement . " " . $niveau;
