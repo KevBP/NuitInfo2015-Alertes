e92bdf6260faf6087d9d0373c59ea76739a790ae
Naum Spaseski <naum.spaseski@edu.univ-fcomte.fr>
2015-12-03 21:54:42 +0100

    legend form

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <div class="row">
         <div class="col-lg-6">
             <form class="form-inline" action="form.php" method="post">
-                <div class="form-group">
-                    <label for="alerte">Name</label>
-                    <input type="text" class="form-control" id="alerte" placeholder="Jane Doe">
-                    <button type="submit" class="btn btn-default">Send invitation</button>
-                </div>
+                <fieldset>
+                <legend>Lancer une alerte !</legend>
+                    <div class="form-group">
+                        <label for="alerte">Name</label>
+                        <input type="text" class="form-control" id="alerte" placeholder="Jane Doe">
+                        <button type="submit" class="btn btn-default">Send invitation</button>
+                    </div>
+                </fieldset>
             </form>
         </div>
         <div class="col-lg-6">
