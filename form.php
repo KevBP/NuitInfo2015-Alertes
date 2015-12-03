ba621d72e2291eb5645c91d175a252c7a755c8ee
N0un <antoinelavier1@gmail.com>
2015-12-03 23:08:51 +0100

    ajout de form.php

--- form.php
+++ form.php
@@ -7,21 +7,13 @@ $con=mysqli_connect("localhost", $user, $pwd,"daddy-staline");
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
-if (!isset($_POST['titre'])
-		|| !isset($_POST['message'])
-		|| !isset($_POST['localisation'])) {
-	echo "bite";
-}
-else {
-			
-	$titre = mysqli_real_escape_string($con,$_POST['titre']);
-	$message = mysqli_real_escape_string($con,$_POST['message']);
-	$localisation = mysqli_real_escape_string($con,$_POST['localisation']);
-	$date_evenement = mysqli_real_escape_string($con,$POST_['date-evenement']);
-	$heure_evenement = mysqli_real_escape_string($con,$_POST['heure-evenement']);
-	$niveau = mysqli_real_escape_string($con,$_POST['niveau']);
 	
-	echo $titre . " " . $message . " " . $localisation . " " . $date_evenement . " " . $date_evenement . " " . $heure_evenement . " " . $niveau;
-}
+$titre = mysqli_real_escape_string($con,$_POST['titre']);
+$message = mysqli_real_escape_string($con,$_POST['message']);
+$localisation = mysqli_real_escape_string($con,$_POST['localisation']);
+$date_evenement = mysqli_real_escape_string($con,$POST_['date-evenement']);
+$heure_evenement = mysqli_real_escape_string($con,$_POST['heure-evenement']);
+$niveau = mysqli_real_escape_string($con,$_POST['niveau']);
+
+echo $titre . " " . $message . " " . $localisation . " " . $date_evenement . " " . $heure_evenement . " " . $niveau;
 ?>
