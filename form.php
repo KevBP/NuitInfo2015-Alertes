baecdd052775282ffbd05fde140e19388590cad0
N0un <antoinelavier1@gmail.com>
2015-12-03 22:33:25 +0100

    modif de form.php

--- form.php
+++ form.php
@@ -1,10 +1,18 @@
 <?php
-$titre = mysqli_real_escape_string($_POST['titre']);
-$message = mysqli_real_escape_string($_POST['message']);
-$localisation = mysqli_real_escape_string($_POST['localisation']);
-$date_evenement = mysqli_real_escape_string($POST_['date_evenement']);
-$heure_evenement = mysqli_real_escape_string($_POST['heure_evenement']);
-$niveau = mysqli_real_escape_string($_POST['niveau']);
-
-echo $titre . " " . $message . " " . $localisation . " " . $date_evenement . " " . $date_evenement . " " . $heure_evenement . " " . $niveau;
+if (isset($_POST['titre'])
+		&& isset($_POST['message'])
+		&& isset($_POST['localisation'])) {
+			echo "bite";
+}
+else {
+			
+	$titre = mysqli_real_escape_string($_POST['titre']);
+	$message = mysqli_real_escape_string($_POST['message']);
+	$localisation = mysqli_real_escape_string($_POST['localisation']);
+	$date_evenement = mysqli_real_escape_string($POST_['date_evenement']);
+	$heure_evenement = mysqli_real_escape_string($_POST['heure_evenement']);
+	$niveau = mysqli_real_escape_string($_POST['niveau']);
+	
+	echo $titre . " " . $message . " " . $localisation . " " . $date_evenement . " " . $date_evenement . " " . $heure_evenement . " " . $niveau;
+}
 ?>
