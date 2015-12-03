2f2547b731a9ab3244d2aceac8feaf4ec569eead
N0un <antoinelavier1@gmail.com>
2015-12-03 22:26:48 +0100

    Modification de form.php avec controle des entrees

--- form.php
+++ form.php
@@ -1,3 +1,10 @@
 <?php
-echo "message = " . $_POST['message']
+$titre = mysqli_real_escape_string($_POST['titre']);
+$message = mysqli_real_escape_string($_POST['message']);
+$localisation = mysqli_real_escape_string($_POST['localisation']);
+$date_evenement = mysqli_real_escape_string($POST_['date_evenement']);
+$heure_evenement = mysqli_real_escape_string($_POST['heure_evenement']);
+$niveau = mysqli_real_escape_string($_POST['niveau']);
+
+echo $titre . " " . $message . " " . $localisation . " " . $date_evenement . " " . $date_evenement . " " . $heure_evenement . " " . $niveau;
 ?>
