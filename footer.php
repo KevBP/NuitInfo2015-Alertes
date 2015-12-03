2aa72d2c2ab16d94e72d39c093b77f33dfb54425
Naum Spaseski <naum.spaseski@edu.univ-fcomte.fr>
2015-12-03 21:27:08 +0100

    refactor footer php

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nomce
+ * Date: 03/12/15
+ * Time: 21:25
+ */
+?>
+
+<img src="http://i.imgur.com/aEew57N.png" alt="Super Chien" class="img-responsive"/>
+Voici la liste des sponsors, elle doit toujours être visible avec leur logo (en bas de page ça convient tout à fait) :
+<a href="http://www.univ-fcomte.fr/"><img src="logo-ufc.jpeg" alt="UFC" class="img-responsive" style="width:200px;"/></a>
+<p class="row">
+<p class="col-lg-2 sponsors">
+    <a href="http://disc.univ-fcomte.fr/"><img src="logo-disc.png" alt="DISC" class="img-responsive" /></a>
+</p>
+<p class="col-lg-2 sponsors">
+    <a href="http://www.smartesting.com/"><img src="logo-smartesting.png" alt="UFC" class="img-responsive"/></a>
+</p>
+<p class="col-lg-2 sponsors">
+    <a href="http://www.jet1oeil.com/"><img src="http://www.jet1oeil.com/wp-content/uploads/2011/09/logo.png" alt="jet1oeil" class="img-responsive" /></a>
+</p>
+<p class="col-lg-2 sponsors">
+    <a href="http://www.onlineformapro.com/"><img src="http://www.onlineformapro.com/templates/corporate_response/images/s5_logo.png" alt="onlineformapro" class="img-responsive" /></a>
+</p>
+<p class="col-lg-2 sponsors">
+    <a href="http://www.onefit-medical.com/"><img src="logo-one-fit.png" alt="onefit medical" class="img-responsive" /></a>
+</p>
+<p class="col-lg-2 sponsors">
+    <a href="http://www.jm-informatique.com/"><img src="http://www.jm-informatique.com/images/logo.png" alt="jm-informatique" class="img-responsive" /></a>
+</p>
+</div>
+</body>
+</html>
