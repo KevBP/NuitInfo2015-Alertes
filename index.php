06cc29d78d44f13e4a278200014fe887479890d3
Naum Spaseski <naum.spaseski@edu.univ-fcomte.fr>
2015-12-03 22:23:47 +0100

    legend form

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
                         <label for="alerte">Localisation</label>
                         <input type="text" class="form-control" id="localisation" name="localisation" placeholder="Localisation">
                         <label for="date-evenement">Date d'évènement</label>
-                        <input type="date" class="form-control" id="date-evenement" name="date-evenement" placeholder="Date d'évènement" data-date-format="yyyy-mm-dd">
+                        <input type="date" class="form-control" id="date-evenement" name="date-evenement" placeholder="Date d'évènement">
+                        <script>$('#date-evenement').datepicker()</script>
                         <label for="heure-evenement">Date d'évènement</label>
                         <input type="time" class="form-control" id="heure-evenement" name="heure-evenement" placeholder="Heure d'évènement">
                         <label for="niveau">Niveau</label>
