1ddc3b8c575062541c75784be3a0a1c152d49522
N0un <antoinelavier1@gmail.com>
2015-12-04 00:43:12 +0100

    Ajout de la redirection

--- form.php
+++ form.php
@@ -7,11 +7,11 @@
 			&& isset($_POST['localisation']) && isset($_POST['date-evenement'])
 			&& isset($_POST['heure-evenement']) && isset($_POST['niveau'])) {
 	
-		$titre = mysqli_real_escape_string($con, $_POST['titre']);
-		$message = mysqli_real_escape_string($con, $_POST['message']);
-		$localisation = mysqli_real_escape_string($con, $_POST['localisation']);
-		$date_evenement = mysqli_real_escape_string($con, $_POST['date-evenement']);
-		$heure_evenement = mysqli_real_escape_string($con, $_POST['heure-evenement']);
+		$titre = mysqli_real_escape_string($con, htmlentities($_POST['titre']));
+		$message = mysqli_real_escape_string($con, htmlentities($_POST['message']));
+		$localisation = mysqli_real_escape_string($con, htmlentities($_POST['localisation']));
+		$date_evenement = mysqli_real_escape_string($con, htmlentities($_POST['date-evenement']));
+		$heure_evenement = mysqli_real_escape_string($con, htmlentities($_POST['heure-evenement']));
 		$niveau = mysqli_real_escape_string($con, $_POST['niveau']);
 		
 		$date = $date_evenement." ".heure_evenement;
