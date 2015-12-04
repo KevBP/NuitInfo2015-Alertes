22e615bcc6897b53f9ca69a65fbb7e269a3e7a18
Naum Spaseski <naum.spaseski@edu.univ-fcomte.fr>
2015-12-04 03:44:09 +0100

    repair scripts

--- ajax-event.php
+++ ajax-event.php
@@ -8,7 +8,7 @@
 require("library.php");
 
 if (isset($_GET['event'])){
-    $local = mysqli_real_escape_string($con, $_GET['event']);
+    $local = mysqli_real_escape_string($con, htmlentities($_GET['event']));
     echo "blabla " . $local ." \n";
     $sql = "SELECT titre_alerte, niveau_alerte,message_alerte FROM ALERTE ORDER BY date_alerte DESC LIMIT $local";
 
