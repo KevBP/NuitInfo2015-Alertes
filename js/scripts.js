c427436819f3f21c508d7fe05c7356a508546d83
kevbp <legauloisfr@gmail.com>
2015-12-04 01:36:39 +0100

    test modal 5 mdr

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
+$('#signalementModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var recipient = button.data('titre-alerte') // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
