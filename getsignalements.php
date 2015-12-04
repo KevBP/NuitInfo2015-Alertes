3fab98d4dc107fde8a4d5d1e2bf8ac0766ca436f
kevbp <legauloisfr@gmail.com>
2015-12-04 06:39:46 +0100

    plus d'ech

--- getsignalements.php
+++ getsignalements.php
@@ -10,10 +10,8 @@ $sql = "SELECT *
              FROM LISTE_SIGNALEMENTS
              WHERE id_alerte = ". $_GET['id'] .")";
 
-echo $sql;
 $req = mysqli_query($con, $sql);
 $result = $req->fetch_assoc();
-echo $result;
 echo json_encode($result);
 
 mysqli_close($con);
