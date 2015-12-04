264b9232b364ded0d3d449a85d1c02fcf43bf23a
kevbp <legauloisfr@gmail.com>
2015-12-04 02:05:04 +0100

    test modal 10 mdr mdr lol

--- test.php
+++ test.php
@@ -72,20 +72,28 @@ $conn->close();
                 echo "0 results";
             }
             ?>
-            <div id="myModal" class="modal fade">
-                <div class="modal-dialog">
+            <div class="modal fade" id="signalementModal" tabindex="-1" role="dialog" aria-labelledby="signalementModalLabel">
+                <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h4 class="modal-title">Confirmation</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="exampleModalLabel">New message</h4>
                         </div>
                         <div class="modal-body">
-                            <p>Do you want to save changes you made to document before closing?</p>
-                            <p class="text-warning"><small>If you don't save, your changes will be lost.</small></p>
+                            <form>
+                                <div class="form-group">
+                                    <label for="recipient-name" class="control-label">Recipient:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="message-text" class="control-label">Message:</label>
+                                    <textarea class="form-control" id="message-text"></textarea>
+                                </div>
+                            </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button type="button" class="btn btn-primary">Send message</button>
                         </div>
                     </div>
                 </div>
