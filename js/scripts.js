440e32d646377e8b41853da6e8db61027f1307c4
kevbp <legauloisfr@gmail.com>
2015-12-04 05:55:56 +0100

    c'est normal tkt

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ $('#alerteModal').on('show.bs.modal', function (event) {
     modal.find('#localisation-alerte').text('Localisation : ' + localisation);
     modal.find('#niveau-alerte').text('Niveau : ' + niveau);
     modal.find('#id-alerte').attr("value", idalerte);
-    alert(idalerte);
 });
 
 $(document).ready(function(){
