b1930c8b70d7412f4410ca2208a893ff7275d172
kevbp <legauloisfr@gmail.com>
2015-12-04 06:38:57 +0100

    espoir peut être 2

--- getsignalements.php
+++ getsignalements.php
@@ -5,7 +5,7 @@ $con = connectToDatabase();
 
 $sql = "SELECT *
         FROM SIGNALEMENT
-        WHERE SIGNALEMENT.id_signalement = ". $_GET['id'] ."
+        WHERE SIGNALEMENT.id_signalement =
             (SELECT LISTE_SIGNALEMENTS.id_signalement
              FROM LISTE_SIGNALEMENTS
              WHERE id_alerte = ". $_GET['id'] .")";
