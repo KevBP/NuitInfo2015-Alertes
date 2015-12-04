cc29c5657935dceb6a3e77620d3a544b9f37251d
kevbp <legauloisfr@gmail.com>
2015-12-04 05:09:29 +0100

    moca index.php

--- index.php
+++ index.php
@@ -59,11 +59,11 @@ $conn->close();
                     echo "<div class=\"list-group\">";
                     while($row = $result->fetch_assoc()) {
                         switch($row['niveau_alerte']){
-                            case "1" : echo '<a href="#" type="button" class="list-group-item" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].' data-localisationalerte="'.$row["localisation_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                            case "2" : echo '<a href="#" type="button" class="list-group-item list-group-item-success" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                            case "3" : echo '<a href="#" type="button" class="list-group-item list-group-item-info" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                            case "4" : echo '<a href="#" type="button" class="list-group-item list-group-item-warning" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'"><h4 class="list-group-item-heading" >'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                            case "5" : echo '<a href="#" type="button" class="list-group-item list-group-item-danger" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'"><h4 class="list-group-item-heading" >'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                            case "1" : echo '<a href="#" type="button" class="list-group-item" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'" data-niveaualerte="'.$row["niveau_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                            case "2" : echo '<a href="#" type="button" class="list-group-item list-group-item-success" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'" data-niveaualerte="'.$row["niveau_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                            case "3" : echo '<a href="#" type="button" class="list-group-item list-group-item-info" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'" data-niveaualerte="'.$row["niveau_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                            case "4" : echo '<a href="#" type="button" class="list-group-item list-group-item-warning" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'" data-niveaualerte="'.$row["niveau_alerte"].'"><h4 class="list-group-item-heading" >'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                            case "5" : echo '<a href="#" type="button" class="list-group-item list-group-item-danger" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'" data-messagealerte="'.$row["message_alerte"].'" data-datealerte="'.$row["date_alerte"].'" data-localisationalerte="'.$row["localisation_alerte"].'" data-niveaualerte="'.$row["niveau_alerte"].'"><h4 class="list-group-item-heading" >'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
                         }
                     }
                     echo "</div>";
@@ -71,19 +71,52 @@ $conn->close();
                     echo "0 results";
                 }
             ?>
-            <button type="button" class="btn btn-default" id="plus-events" aria-label="Plus" data-nb="5">Plus... :)</button>
-
             <div class="modal fade" id="alerteModal" tabindex="-1" role="dialog" aria-labelledby="alerteModalLabel">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="alerteModalLabel">New message</h4>
+                            <h3 class="modal-title" id="alerteModalLabel">New message</h3>
                         </div>
                         <div class="modal-body">
-                            <p id="date-alerte"></p>
-                            <p id="localisation-alerte"></p>
-                            <p id="message-alerte"></p>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <p id="date-alerte"></p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p id="localisation-alerte"></p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p id="niveau-alerte"></p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <p id="message-alerte"></p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <form class="form" action="form.php" method="post">
+                                        <fieldset>
+                                            <legend>Lancer une signalisation !</legend>
+                                            <div class="form-group">
+                                                <label for="nom_sig">Nom Prénom</label>
+                                                <input type="text" class="form-control" id="nom_sig" name="nom_sig" placeholder="Nom Prénom de la personne concernée">
+                                                <label for="age_sig">Age</label>
+                                                <input type="text" class="form-control" id="age_sig" name="age_sig" placeholder="Age">
+                                                <label for="sexe_sig">Sexe</label>
+                                                <input type="text" class="form-control" id="sexe_sig" name="sexe_sig" placeholder="Sexe">
+                                                <label for="description_sig">Description</label>
+                                                <input type="text" class="form-control" id="description_sig" name="description_sig" placeholder="Description">
+                                                <span class="input-group-btn">
+                                                    <button type="submit" class="btn btn-default">Envoyer signalement !</button>
+                                                </span>
+                                            </div>
+                                        </fieldset>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
