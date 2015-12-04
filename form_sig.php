3b9c02c0cfde307769982d11a11591258dca7237
N0un <antoinelavier1@gmail.com>
2015-12-04 05:46:59 +0100

    commit

--- form_sig.php
+++ form_sig.php
@@ -13,8 +13,34 @@
 		$age = mysqli_real_escape_string($con, htmlentities($_POST['age_sig']));
 		$sexe = mysqli_real_escape_string($con, htmlentities($_POST['sexe_sig']));
 		$description = mysqli_real_escape_string($con, htmlentities($_POST['description_sig']));
+		echo $_POST['id-alerte'];
+		//echo "nom = " . $nom . ", age = " . $age . ", sexe = " . $sexe . ", " . $description;
+		if (strlen($nom) < 1) {
+			$erreurs = true;
+			echo "Erreur nom";
+		}
 		
-		echo "nom = " . $nom . ", age = " . $age . ", sexe = " . $sexe . ", " . $description;
+		if (!is_int($age) || $age < 0 || $age > 110) {
+			$erreurs = true;
+			echo "Erreur age";
+		}
+		
+		if ($sexe != "H" || $sexe !="F") {
+			$erreurs = true;
+			echo "Erreur sexe";
+		}
+		
+		if (strlen($description) > 2048) {
+			$erreurs = true;
+			echo "Erreur description";
+		}
+		
+		if (!$erreurs) {
+			$sql = "INSERT INTO SIGNALEMENT
+					(nom_prenom_signalement, age_signalement, sexe_signalement, description_signalement)
+					VALUES ('$nom', '$age', '$sexe', '$description')";
+		}
+		
 	}
 	else {
 		echo "Erreur POST";
