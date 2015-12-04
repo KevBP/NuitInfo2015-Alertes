1f8d8e838df5ea2a28acc9ea35944cedfc1c7855
N0un <antoinelavier1@gmail.com>
2015-12-04 05:37:08 +0100

    Ajout du formulaire de signalement

---
+++ form_sig.php
@@ -0,0 +1,22 @@
+<?php
+	require("library.php");
+	
+	$con = connectToDatabase();
+	
+	$erreurs = false;
+	
+	if ($con != NULL && isset($_POST['nom_sig'])
+			&& isset($_POST['age_sig']) && isset($_POST['sexe_sig'])
+			&& isset($_POST['description_sig'])) {
+			
+		$nom = mysqli_real_escape_string($con, htmlentities($_POST['nom_sig']));
+		$age = mysqli_real_escape_string($con, htmlentities($_POST['age_sig']));
+		$sexe = mysqli_real_escape_string($con, htmlentities($_POST['sexe_sig']));
+		$description = mysqli_real_escape_string($con, htmlentities($_POST['description_sig']));
+		
+		echo "nom = " . $nom;
+	}
+	else {
+		echo "Erreur POST";
+	}
+?>
