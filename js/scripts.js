0ae091a22c425b376d07a8dd94eb482f03acb198
kevbp <legauloisfr@gmail.com>
2015-12-04 03:53:04 +0100

    date

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $('#alerteModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
+    var button = $(event.relatedTarget);
     var titre = button.data('titrealerte');
     var message = button.data('messagealerte');
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var dalerte = button.data('datealerte');
     var modal = $(this);
     modal.find('.modal-title').text('Alerte ' + titre);
     alert(message);
