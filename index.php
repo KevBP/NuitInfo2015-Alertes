a30e3206b53c004f3d7f3bc6884669feccfc815b
Naum Spaseski <naum.spaseski@edu.univ-fcomte.fr>
2015-12-04 05:14:43 +0100

    repair scripts

--- index.php
+++ index.php
@@ -71,6 +71,8 @@ $conn->close();
                     echo "0 results";
                 }
             ?>
+            <button type="button" class="btn btn-default" id="plus-events">Plus.. :)</button>
+
             <div class="modal fade" id="alerteModal" tabindex="-1" role="dialog" aria-labelledby="alerteModalLabel">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
