04c0655e2bf2920eb962560ed1f4c7b7b48fa6e1
kevbp <legauloisfr@gmail.com>
2015-12-04 03:40:57 +0100

    rename whatever

--- test.php
+++ test.php
@@ -60,11 +60,11 @@ $conn->close();
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
                     switch($row['niveau_alerte']){
-                        case "1" : echo '<a href="#" type="button" class="list-group-item" data-toggle="modal" data-target="#alerteModal" data-whatever="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" data-whatever="'.$row["titre_alerte"].'">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                        case "2" : echo '<a href="#" type="button" class="list-group-item list-group-item-success" data-toggle="modal" data-target="#alerteModal" data-whatever="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" data-whatever="'.$row["titre_alerte"].'">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                        case "3" : echo '<a href="#" type="button" class="list-group-item list-group-item-info" data-toggle="modal" data-target="#alerteModal" data-whatever="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" data-whatever="'.$row["titre_alerte"].'">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                        case "4" : echo '<a href="#" type="button" class="list-group-item list-group-item-warning" data-toggle="modal" data-target="#alerteModal" data-whatever="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" data-whatever="'.$row["titre_alerte"].'">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
-                        case "5" : echo '<a href="#" type="button" class="list-group-item list-group-item-danger" data-toggle="modal" data-target="#alerteModal" data-whatever="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" data-whatever="'.$row["titre_alerte"].'">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                        case "1" : echo '<a href="#" type="button" class="list-group-item" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                        case "2" : echo '<a href="#" type="button" class="list-group-item list-group-item-success" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                        case "3" : echo '<a href="#" type="button" class="list-group-item list-group-item-info" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading">'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                        case "4" : echo '<a href="#" type="button" class="list-group-item list-group-item-warning" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" >'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
+                        case "5" : echo '<a href="#" type="button" class="list-group-item list-group-item-danger" data-toggle="modal" data-target="#alerteModal" data-titrealerte="'.$row["titre_alerte"].'"><h4 class="list-group-item-heading" >'. $row["titre_alerte"] .'</h4><p class="list-group-item-text">' . $row["message_alerte"] . '</p></a>'; break;
                     }
                 }
                 echo "</div>";
@@ -80,7 +80,7 @@ $conn->close();
                             <h4 class="modal-title" id="alerteModalLabel">New message</h4>
                         </div>
                         <div class="modal-body">
-
+                            <p id="description-alerte"></p>
                         </div>
                     </div>
                 </div>
